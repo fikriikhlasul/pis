2fb2b2827464a13c9e7f99d356a44189f698c3e1
vkree <fikriikhlasul82@gmail.com>
2020-07-17 21:34:24 +0700

    Fix Minor Issue ke 9

--- application/views/auth/blocked.php
+++ application/views/auth/blocked.php
@@ -10,20 +10,10 @@
     <meta name="author" content="">
 
     <title>Access Blocked</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="<?= base_url('sbadmin2/assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="<?= base_url('sbadmin2/assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
-
+    <link href="<?= base_url('asset_authen/blocked/'); ?>css/blocked.min.css" rel="stylesheet">
 </head>
-
 <body id="page-top">
-
-   
-                <!-- Begin Page Content -->
                 <div class="container">
                     <br/>
                     <br/>
@@ -55,17 +45,8 @@
                     </div>
 
                 </div>
-                <!-- /.container-fluid -->
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('sbadmin2/assets/'); ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url('sbadmin2/assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('sbadmin2/assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('sbadmin2/assets/'); ?>js/sb-admin-2.min.js"></script>
+        
+    <script src="<?= base_url('asset_authen/blocked/'); ?>js/blocked.js"></script>
 
 </body>
 
